ef1997506c6f9e9f8c91f9434fe2e8909f2f0066
vikasyedave <84763974+vikasyedave@users.noreply.github.com>
2021-12-10 09:30:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,197 @@
+
+if (typeof jQuery === 'undefined') {
+    throw new Error('Bootstrap\'s JavaScript requires jQuery')
+}
+
++function ($) {
+    'use strict';
+    var version = $.fn.jquery.split(' ')[0].split('.')
+    if ((version[0] < 2 && version[1] < 9) || (version[0] == 1 && version[1] == 9 && version[2] < 1) || (version[0] > 2)) {
+        throw new Error('Bootstrap\'s JavaScript requires jQuery version 1.9.1 or higher, but lower than version 3')
+    }
+}(jQuery);
+
+
+
+
++function ($) {
+    'use strict';
+
+
+
+    function transitionEnd() {
+        var el = document.createElement('bootstrap')
+
+        var transEndEventNames = {
+            WebkitTransition : 'webkitTransitionEnd',
+            MozTransition    : 'transitionend',
+            OTransition      : 'oTransitionEnd otransitionend',
+            transition       : 'transitionend'
+        }
+
+        for (var name in transEndEventNames) {
+            if (el.style[name] !== undefined) {
+                return { end: transEndEventNames[name] }
+            }
+        }
+
+        return false // explicit for ie8 (  ._.)
+    }
+
+    // http://blog.alexmaccaw.com/css-transitions
+    $.fn.emulateTransitionEnd = function (duration) {
+        var called = false
+        var $el = this
+        $(this).one('bsTransitionEnd', function () { called = true })
+        var callback = function () { if (!called) $($el).trigger($.support.transition.end) }
+        setTimeout(callback, duration)
+        return this
+    }
+
+    $(function () {
+        $.support.transition = transitionEnd()
+
+        if (!$.support.transition) return
+
+        $.event.special.bsTransitionEnd = {
+            bindType: $.support.transition.end,
+            delegateType: $.support.transition.end,
+            handle: function (e) {
+                if ($(e.target).is(this)) return e.handleObj.handler.apply(this, arguments)
+            }
+        }
+    })
+
+}(jQuery);
+
+
+
+
++function ($) {
+    'use strict';
+
+    // ALERT CLASS DEFINITION
+    // ======================
+
+    var dismiss = '[data-dismiss="alert"]'
+    var Alert   = function (el) {
+        $(el).on('click', dismiss, this.close)
+    }
+
+    Alert.VERSION = '3.3.6'
+
+    Alert.TRANSITION_DURATION = 150
+
+    Alert.prototype.close = function (e) {
+        var $this    = $(this)
+        var selector = $this.attr('data-target')
+
+        if (!selector) {
+            selector = $this.attr('href')
+            selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
+        }
+
+        var $parent = $(selector)
+
+        if (e) e.preventDefault()
+
+        if (!$parent.length) {
+            $parent = $this.closest('.alert')
+        }
+
+        $parent.trigger(e = $.Event('close.bs.alert'))
+
+        if (e.isDefaultPrevented()) return
+
+        $parent.removeClass('in')
+
+        function removeElement() {
+            // detach from parent, fire event then clean up data
+            $parent.detach().trigger('closed.bs.alert').remove()
+        }
+
+        $.support.transition && $parent.hasClass('fade') ?
+            $parent
+                .one('bsTransitionEnd', removeElement)
+                .emulateTransitionEnd(Alert.TRANSITION_DURATION) :
+            removeElement()
+    }
+
+
+    // ALERT PLUGIN DEFINITION
+    // =======================
+
+    function Plugin(option) {
+        return this.each(function () {
+            var $this = $(this)
+            var data  = $this.data('bs.alert')
+
+            if (!data) $this.data('bs.alert', (data = new Alert(this)))
+            if (typeof option == 'string') data[option].call($this)
+        })
+    }
+
+    var old = $.fn.alert
+
+    $.fn.alert             = Plugin
+    $.fn.alert.Constructor = Alert
+
+
+    // ALERT NO CONFLICT
+    // =================
+
+    $.fn.alert.noConflict = function () {
+        $.fn.alert = old
+        return this
+    }
+
+
+
+
+    $(document).on('click.bs.alert.data-api', dismiss, Alert.prototype.close)
+
+}(jQuery);
+
+/
+
++function ($) {
+    'use strict';
+
+    // BUTTON PUBLIC CLASS DEFINITION
+    // ==============================
+
+    var Button = function (element, options) {
+        this.$element  = $(element)
+        this.options   = $.extend({}, Button.DEFAULTS, options)
+        this.isLoading = false
+    }
+
+    Button.VERSION  = '3.3.6'
+
+    Button.DEFAULTS = {
+        loadingText: 'loading...'
+    }
+
+    Button.prototype.setState = function (state) {
+        var d    = 'disabled'
+        var $el  = this.$element
+        var val  = $el.is('input') ? 'val' : 'html'
+        var data = $el.data()
+
+        state += 'Text'
+
+        if (data.resetText == null) $el.data('resetText', $el[val]())
+
+        // push to event loop to allow forms to submit
+        setTimeout($.proxy(function () {
+            $el[val](data[state] == null ? this.options[state] : data[state])
+
+            if (state == 'loadingText') {
+                this.isLoading = true
+                $el.addClass(d).attr(d, d)
+            } else if (this.isLoading) {
+                this.isLoading = false
+                $el.removeClass(d).removeAttr(d)
+            }
+        }, this), 0)
+    }
